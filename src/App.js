0266433025792401978589136138624b32b04496
User <user@MacBook-Air-cua-User.local>
2019-10-24 15:20:53 +0700

    update fixed

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import NavBar from "./components/NavBar";
-import Pagination from "./components/Pagination";
+import Paginations from "./components/Pagination";
 import Body from "./components/Body";
 import Modal from "./components/Modal";
 import Footer from "./components/Footer";
 
 function App() {
   const [token, setToken] = useState(null);
-  const [githubIssues, setGithubIssues] = useState([])
-  const [pagination, setPagination] = useState(1);
+  const [reactRepo, setReactRepo] = useState({})
+  const [githubIssues, setGithubIssues] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
   const [modalOpen, setModalOpen] = useState(false);
+
+  console.log(reactRepo)
   
   useEffect(() => {
     const clientId = `05449736a72133433d33`;
@@ -40,23 +43,43 @@ function App() {
     }
   }, []);
 
-  useEffect(()=>{getGithubIssuesData()},[])
+  useEffect(()=>{
+    getGithubIssuesData()
+  },[])
+
+  // get react issues
   const getGithubIssuesData = async () => {
 
-    const url = `https://api.github.com/repos/vmg/redcarpet/issues?page=${pagination}?per_page=20`
+    const url = `https://api.github.com/repos/facebook/react/issues?page=${currentPage}?per_page=20`
     const response = await fetch(url)
+
+    // get header link
+    const link = await response.headers.get('link')
+
     const githubIssuesData = await response.json()
     setGithubIssues(githubIssuesData);
-    console.log("Github issues", githubIssuesData);
   }
 
+
+  // get react repo to find total issues
+  const getGithubReactRepo = async () => {
+    const url = `https://api.github.com/repos/facebook/react`
+    const response = await fetch(url)
+    const reactRepoData = await response.json();
+    setReactRepo(reactRepoData)
+  }
 
   return (
     <div className="App">
       <h1>Hello World!</h1>
         <NavBar />
       <section className="section">
-        <Pagination />
+        <Paginations 
+          reactRepo = {reactRepo}
+          currentPage = {currentPage}
+          setCurrentPage = {setCurrentPage}
+          
+        />
         <Body />
         <Modal />
         <Footer />
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from "react";
 import Pagination from "react-bootstrap/Pagination";
 
-export default function Pagination() {
-    return (
-        <div>
-            
-        </div>
-    )
+export default function Paginations(props) {
+    let totalPage = props.reactRepo.open_issues/20;
+    let pages = [1];
+    for (let i = 1; i <= totalPage; i++) pages.push(i)
+
+  return (
+    <div>
+      <Pagination>
+        <Pagination.First disables={props.currentPage === 1} onClick={()=>props.setCurrentPage(1)}/>
+        <Pagination.Prev disables={props.currentPage === 1} onClick={()=>props.setCurrentPage(props.currentPage-1)}/>
+     
+        {pages.map(el => {
+            if (el >= props.currentPage -2 && el <= props.currentPage + 2)
+            return <Pagination.Item active={el === props.currentPage} onClick={()=>props.setCurrentPage(el)}>{el}</Pagination.Item>
+        })}
+       
+       <Pagination.Next disables={props.currentPage === totalPage} onClick={()=>props.setCurrentPage(props.currentPage+1)}/>
+      <Pagination.Last disables={props.currentPage === totalPage} onClick={()=>props.setCurrentPage(totalPage)}/>
+      </Pagination>
+    </div>
+  );
 }
