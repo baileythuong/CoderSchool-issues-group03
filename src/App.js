96ffcde52aa27f78f7ba731216fbe7db4730279f
Bailey Nguyen <baileythuong@gmail.com>
2019-10-24 18:18:26 +0700

    modified app.js

--- src/App.js
+++ src/App.js
@@ -11,54 +11,52 @@ import Footer from "./components/Footer";
 function App() {
   const [token, setToken] = useState(null);
   const [githubIssues, setGithubIssues] = useState([])
-  const [pagination, setPagination] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
   const [modalOpen, setModalOpen] = useState(false);
   
-  // useEffect(() => {
-  //   const clientId = `05449736a72133433d33`;
+  useEffect(() => {
+    const clientId = `05449736a72133433d33`;
+    const secretKey = `d0115c0e09c202d8e50ff6260e374294c187ab5a`
 
-  //   const existingToken = sessionStorage.getItem("token");
-  //   const accessToken =
-  //     window.location.search.split("=")[0] === "?access_token"
-  //       ? window.location.search.split("=")[1]
-  //       : null;
+    const existingToken = sessionStorage.getItem("token");
+    const accessToken =
+      window.location.search.split("=")[0] === "?access_token"
+        ? window.location.search.split("=")[1]
+        : null;
 
-  //   if (!accessToken && !existingToken) {
-  //     window.location.replace(
-  //       `https://github.com/login/oauth/authorize?scope=user:email,repo&client_id=${clientId}`
-  //     );
-  //   }
+    if (!accessToken && !existingToken) {
+      window.location.replace(
+        `https://github.com/login/oauth/authorize?scope=user:email,repo&client_id=${clientId}&client_secret=${secretKey}`
+      );
+    }
 
-  //   if (accessToken) {
-  //     // console.log(`New accessToken: ${accessToken}`);
+    if (accessToken) {
+      // console.log(`New accessToken: ${accessToken}`);
 
-  //     sessionStorage.setItem("token", accessToken);
-  //     setToken(accessToken);
-  //   }
+      sessionStorage.setItem("token", accessToken);
+      setToken(accessToken);
+    }
 
-  //   if (existingToken) {
-  //     setToken(existingToken);
-  //   }
-  // }, []);
+    if (existingToken) {
+      setToken(existingToken);
+    }
+  }, []);
 
-  // useEffect(()=>{
-  //   getGithubIssuesData()
-  // },[])
+  useEffect(()=>{
+    getGithubIssuesData()
+  },[])
 
-  // const getGithubIssuesData = async () => {
-
-  //   const url = `https://api.github.com/repos/vmg/redcarpet/issues?page=${pagination}?per_page=20`
-  //   const response = await fetch(url)
-  //   const githubIssuesData = await response.json()
-  //   setGithubIssues(githubIssuesData);
-  //   console.log("Github issues", githubIssuesData);
-  //   console.log("hello")
-  // }
+  const getGithubIssuesData = async () => {
+    const url = `https://api.github.com/repos/facebook/react/issues?page=${currentPage}?per_page=20`
+    const response = await fetch(url)
+    const githubIssuesData = await response.json()
+    setGithubIssues(githubIssuesData);
+    console.log("github issues data", githubIssuesData);
+  }
 
 
   return (
     <div className="App">
-      <h1>Hello World!</h1>
         <NavBar />
       <section className="section">
         <Pagination />
