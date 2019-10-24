75559ad47956ed7ead25988c73ea23db3a22f897
Bailey Nguyen <baileythuong@gmail.com>
2019-10-24 15:19:57 +0700

    fix bootstrap problem

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
 import NavBar from "./components/NavBar";
@@ -13,42 +14,46 @@ function App() {
   const [pagination, setPagination] = useState(1);
   const [modalOpen, setModalOpen] = useState(false);
   
-  useEffect(() => {
-    const clientId = `05449736a72133433d33`;
+  // useEffect(() => {
+  //   const clientId = `05449736a72133433d33`;
 
-    const existingToken = sessionStorage.getItem("token");
-    const accessToken =
-      window.location.search.split("=")[0] === "?access_token"
-        ? window.location.search.split("=")[1]
-        : null;
+  //   const existingToken = sessionStorage.getItem("token");
+  //   const accessToken =
+  //     window.location.search.split("=")[0] === "?access_token"
+  //       ? window.location.search.split("=")[1]
+  //       : null;
 
-    if (!accessToken && !existingToken) {
-      window.location.replace(
-        `https://github.com/login/oauth/authorize?scope=user:email,repo&client_id=${clientId}`
-      );
-    }
+  //   if (!accessToken && !existingToken) {
+  //     window.location.replace(
+  //       `https://github.com/login/oauth/authorize?scope=user:email,repo&client_id=${clientId}`
+  //     );
+  //   }
 
-    if (accessToken) {
-      // console.log(`New accessToken: ${accessToken}`);
+  //   if (accessToken) {
+  //     // console.log(`New accessToken: ${accessToken}`);
 
-      sessionStorage.setItem("token", accessToken);
-      setToken(accessToken);
-    }
+  //     sessionStorage.setItem("token", accessToken);
+  //     setToken(accessToken);
+  //   }
 
-    if (existingToken) {
-      setToken(existingToken);
-    }
-  }, []);
+  //   if (existingToken) {
+  //     setToken(existingToken);
+  //   }
+  // }, []);
 
-  useEffect(()=>{getGithubIssuesData()},[])
-  const getGithubIssuesData = async () => {
+  // useEffect(()=>{
+  //   getGithubIssuesData()
+  // },[])
 
-    const url = `https://api.github.com/repos/vmg/redcarpet/issues?page=${pagination}?per_page=20`
-    const response = await fetch(url)
-    const githubIssuesData = await response.json()
-    setGithubIssues(githubIssuesData);
-    console.log("Github issues", githubIssuesData);
-  }
+  // const getGithubIssuesData = async () => {
+
+  //   const url = `https://api.github.com/repos/vmg/redcarpet/issues?page=${pagination}?per_page=20`
+  //   const response = await fetch(url)
+  //   const githubIssuesData = await response.json()
+  //   setGithubIssues(githubIssuesData);
+  //   console.log("Github issues", githubIssuesData);
+  //   console.log("hello")
+  // }
 
 
   return (
