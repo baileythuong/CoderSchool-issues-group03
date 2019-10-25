a80f5fee30fcfcdaf2c892dadc2091cf0ecdc382
Ricardo Cuevas Escalante <ricardocuevasescalante@Ricardos-MacBook-Pro.local>
2019-10-25 18:16:09 +0700

    ricardo update friday end

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,56 +1,50 @@
 import React from "react";
 import { Modal, Button, Badge } from "react-bootstrap";
-const ReactMarkdown = require('react-markdown')
-
+const ReactMarkdown = require("react-markdown");
 
 export default function MyModal(props) {
-
-  console.log('sadfasf',props.comments)
-  
   return (
     <>
-      <Modal 
-      show={props.show} 
-      onHide={props.onClick}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
+      <Modal
+        show={props.show}
+        onHide={props.onClick}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
       >
         <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">{props.issue.title}
-          <h3><Badge variant="success">{props.issue.state}</Badge></h3>
+          <Modal.Title id="contained-modal-title-vcenter">
+            {props.issue.title}
+            <h3>
+              <Badge variant="success">{props.issue.state}</Badge>
+            </h3>
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <samp>
-          <ReactMarkdown
-          source={props.issue.body}
-          escapeHtml={false}
-          readOnly
-        />
-        </samp>
+            <ReactMarkdown
+              source={props.issue.body}
+              escapeHtml={false}
+              className="body-image"
+              readOnly
+            />
+          </samp>
         </Modal.Body>
-        {props.comments.map((comment) => {
+        {props.comments.map(comment => {
           return (
             <div>
-          <Modal.Body className="commentsarea">
-          {comment.user.login}
+              <Modal.Body className="commentsarea">
+                {comment.user.login}
 
-          <ReactMarkdown
-          source={comment.body}
-          escapeHtml={false}
-          readOnly />
-                  </Modal.Body>
-
-          </div>
-          )
+                <ReactMarkdown
+                  source={comment.body}
+                  escapeHtml={false}
+                  readOnly
+                />
+              </Modal.Body>
+            </div>
+          );
         })}
-        
-        
-          
-
-         
-
       </Modal>
     </>
   );
